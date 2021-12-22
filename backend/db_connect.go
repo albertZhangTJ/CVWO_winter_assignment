62f3d72db1ed7ae349b802f761bb71697fad3904
albert <tongjunz@outlook.com>
2021-12-23 05:22:41 +0800

    122320210522

--- backend/db_connect.go
+++ backend/db_connect.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func execute_sql(cmd string) string {
-	//TODO
+
+	//In actual engineering setup this should not be hard-coded
+	//But I really don't wanna go through the trouble of having this thing setup in a separate config file
+	db, err := sql.Open("mysql", "CVWO:cvwo_winter_assignment@/cvwo_winter_assignment")
+
 	return ""
 }
