4ac5a6f2baed5e491d5d911b24f70fdd44414728
albert <tongjunz@outlook.com>
2021-12-29 05:31:12 +0800

    122920210531

--- backend/event_listener.go
+++ backend/event_listener.go
@@ -22,8 +22,8 @@ func init_listener(wg *sync.WaitGroup, port int) {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("register", register)
 	http.HandleFunc("/logout", logout)
-	//http.HandleFunc("/view_month", view_month)
-	//http.HandleFunc("/view_day", view_day)
+	http.HandleFunc("/view_month", view_month)
+	http.HandleFunc("/view_day", view_day)
 	//http.HandleFunc("/create_event", create_event)
 
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
