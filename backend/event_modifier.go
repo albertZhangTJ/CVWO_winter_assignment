6ad4497f81eb903308f1cdeb40e853eedc7b4973
albert <tongjunz@outlook.com>
2022-01-02 12:14:12 +0800

    010220211214

--- backend/event_modifier.go
+++ backend/event_modifier.go
@@ -48,3 +48,42 @@ func create_event(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(strconv.Itoa(len(parsed_payload)) + " events added"))
 }
+
+//This function is called when a user adds an event
+//http request body: session_id,VCALENDAR
+//http response body: Number of events added
+func delete_event(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	var content string = buf.String()
+
+	data := split_after_first_comma(content)
+
+	var ssid string = data[0]
+	var payload string = data[1]
+
+	var username string = ""
+	lck.Lock()
+	for i := 0; i < len(logged_users); i++ {
+		if logged_users[i].session_id == ssid {
+			username = logged_users[i].username
+			break
+		}
+	}
+	lck.Unlock()
+
+	if username == "" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("User appears to be not logged in"))
+		return
+	}
+
+	var parsed_payload [][]string = vcalendar_parser(payload)
+	for i := 0; i < len(parsed_payload); i++ {
+		execute_sql("UPDATE events SET isdeleted='Y' WHERE username='"+username+"',eventname='"+parsed_payload[i][2]+"', dtstart='"+parsed_payload[i][4]+"',dtend='"+parsed_payload[i][5]+"';", 0, false)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(strconv.Itoa(len(parsed_payload)) + " events deleted"))
+}
